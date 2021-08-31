7c0b50ed1a28f2eb7d9319e07238fabd3d02403b
rachit <bhatiarach@hotmail.com>
2021-08-31 00:04:15 -0400

    ContainerWithMostWater problem better solution

--- src/main/java/practice/ContainerWithMostWater.java
+++ src/main/java/practice/ContainerWithMostWater.java
@@ -1,35 +1,25 @@
 package practice;
 
-import java.util.*;
-
 public class ContainerWithMostWater {
     public static void main(String[] args) {
-        maxArea(new int[]{1, 8, 6, 2, 5, 4, 8, 3, 7});
+        System.out.println(maxArea(new int[]{1, 1}));
     }
 
     private static int maxArea(int[] height) {
         int target = 0;
-        TreeMap<Integer, Integer> treeMap = new TreeMap<>(Collections.reverseOrder());
-        for (int i =0;i< height.length;i++){
-            treeMap.put(height[i], i);
-        }
-        Set entrySet = treeMap.entrySet();
-        Iterator iterator = entrySet.iterator();
-        int x=0;
-        int key=0;
-        while (iterator.hasNext()){
-            Map.Entry m = (Map.Entry) iterator.next();
-            int tempKey = (int) m.getKey();
-
-            if(x>0){
-               int tempTarget =  tempKey * (Math.abs(treeMap.get(tempKey) - treeMap.get(key)));
-               if(tempTarget> target){
-                   target = tempTarget;
-               }
+        int i = 0, j = height.length - 1;
+        while (i < height.length) {
+            int temp;
+            if (height[i] > height[j]) {
+                temp = height[j] * (j - i);
+                j--;
+            } else {
+                temp = height[i] * (j - i);
+                i++;
             }
-            x++;
-            key = tempKey;
-
+            if (target < temp) {
+                target = temp;
+            }
         }
         return target;
     }
